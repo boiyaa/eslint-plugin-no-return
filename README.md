d8b549caf855d5153d0cd1760544b6b4de70579f
boiyaa <boiyaa@hotmail.com>
2018-04-01 02:19:53 +0900

    Add plugin and rule

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # eslint-plugin-no-return
+
+ESLint plugin to disable return statement
+
+## Installation
+
+You'll first need to install [ESLint](http://eslint.org):
+
+```
+$ npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-no-return`:
+
+```
+$ npm install eslint-plugin-no-return --save-dev
+```
+
+**Note:** If you installed ESLint globally (using the `-g` flag) then you must also install `eslint-plugin-no-return` globally.
+
+## Usage
+
+Add `no-return` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "no-return"
+    ]
+}
+```
+
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+    "rules": {
+        "no-return/no-return": 2
+    }
+}
+```
+
+## Supported Rule
+
+* `no-return` - disallow `return` statements
---
+++ tests/lib/rules/no-return.js
@@ -0,0 +1,52 @@
+/**
+ * @fileoverview Tests for no-return rule.
+ * @author boiyaa
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/no-return"),
+
+    RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 6 } });
+ruleTester.run("no-return", rule, {
+
+    valid: [
+        {
+            code: "var foo = bar => bar * 2;"
+        }
+    ],
+
+    invalid: [
+        {
+            code: "function foo(bar) { var baz = bar * 2; return baz; }",
+            errors: [{
+                message: "Unexpected return, use concise body syntax instead.",
+                type: "ReturnStatement"
+            }]
+        },
+        {
+            code: "var foo = function (bar) { var baz = bar * 2; return baz; };",
+            errors: [{
+                message: "Unexpected return, use concise body syntax instead.",
+                type: "ReturnStatement"
+            }]
+        },
+        {
+            code: "var foo = bar => { var baz = bar * 2; return baz; };",
+            errors: [{
+                message: "Unexpected return, use concise body syntax instead.",
+                type: "ReturnStatement"
+            }]
+        }
+    ]
+});
